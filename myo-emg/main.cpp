71af22d7278060582f8e0ccb9d27d5aad31ccbd9
naoyakouda <nashu5667@gmail.com>
2017-05-10 13:39:23 +0900

    Initial Commit

---
+++ myo-emg/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  myo-emg
+//
+//  Created by 幸田尚也 on 2017/05/10.
+//  Copyright (c) 2017年 幸田尚也. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
